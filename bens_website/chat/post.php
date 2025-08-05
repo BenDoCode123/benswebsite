f924d24c9bc2d256ca8b2681235c806dd7157147
BenDoCode123 <alpineendurancefit@gmail.com>
2025-08-05 14:24:16 +0100

    Delete bens_website directory

--- bens_website/chat/post.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'db.php';
-$user = strip_tags($_POST['user']);
-$message = strip_tags($_POST['message']);
-if ($user && $message) {
-  $stmt = $db->prepare("INSERT INTO messages (user, message) VALUES (?, ?)");
-  $stmt->execute([$user, $message]);
-}
-?>
